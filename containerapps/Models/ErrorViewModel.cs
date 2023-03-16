64991206ab58a96173a2e326b6a5261693e56d8a
Kishore <Kishoresm.sm@gmail.com>
2023-03-16 22:38:56 +0530

    first commit

---
+++ containerapps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace containerapps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
